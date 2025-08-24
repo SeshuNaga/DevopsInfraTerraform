ee1b3f5ae370474aaed7ec631a929afba2e6d775
SeshuNaga <99394429+SeshuNaga@users.noreply.github.com>
2025-08-24 08:30:35 +0000

    Added storage Block

--- jenkins.tf
+++ jenkins.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "jenkins_server" {
     key_name = "seshu"
     associate_public_ip_address = true
     security_groups = [ aws_security_group.devops_security_group.id ]
-    
+    root_block_device {
+    volume_size = 30        # 30 GiB
+    volume_type = "gp3"     # General Purpose SSD
+    iops        = 3000      # IOPS for gp3 (default: 3000)
+    throughput  = 125       # Throughput in MB/s (optional, default 125)
+    encrypted   = false     # keep unencrypted since you mentioned "Not encrypted"
+  }
   
 }
