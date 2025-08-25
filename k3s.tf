a3d67c6cfdd63feeb2daf86afa591554d17764df
SeshuNaga <99394429+SeshuNaga@users.noreply.github.com>
2025-08-25 16:16:28 +0000

    Added k3's tf

---
+++ k3s.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "k3s_server" {
+    ami = "ami-04726aaccec3fc9a2"
+    instance_type = "t2.micro"
+    associate_public_ip_address = true
+    security_groups = [ aws_security_group.devops_security_group.id ]
+    subnet_id = aws_subnet.devops_public_subnet.id
+    key_name = "seshu"
+    root_block_device {
+        volume_size = 30        # 30 GiB
+        volume_type = "gp3"     # General Purpose SSD
+        iops        = 3000      # IOPS for gp3 (default: 3000)
+        throughput  = 125       # Throughput in MB/s (optional, default 125)
+        encrypted   = false     # keep unencrypted since you mentioned "Not encrypted"
+    }
+
+  
+}
