7d80b6cbf267f6d3b9c59173635415c8d87752d7
SeshuNaga <99394429+SeshuNaga@users.noreply.github.com>
2025-08-30 16:33:06 +0000

    Added terraform backend with s3 and dynamo DB

--- backend.tf
+++ backend.tf
@@ -3,5 +3,8 @@ terraform {
     bucket = "seshubucketnaga"
      key    = "env/dev/terraform.tfstate"  # Path in the bucket (acts like folder)
     region = "ap-south-1"  
+    dynamodb_table = "terraform"
+    
+
   }
 }
